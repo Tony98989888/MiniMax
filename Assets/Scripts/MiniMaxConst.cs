d8e25e50c0b72981f18f71d4cbbc85498f654c6a
Tony98989888 <104243086+Tony98989888@users.noreply.github.com>
2023-06-19 10:26:20 +0800

    Project init

---
+++ Assets/Scripts/MiniMaxConst.cs
@@ -0,0 +1,10 @@
+public class MiniMaxConst
+{
+    public static readonly string GroupID = "1686274429654775";
+    public static readonly string API = "https://api.minimax.chat/v1/text/chatcompletion";
+
+    public static readonly string APIKEY =
+        "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJOYW1lIjoiVG9ueSIsIlN1YmplY3RJRCI6IjE2ODYyNzQ0Mjk2NzY2OTYiLCJQaG9uZSI6Ik1UZzJORFV4TnpFM05qWT0iLCJHcm91cElEIjoiMTY4NjI3NDQyOTY1NDc3NSIsIlBhZ2VOYW1lIjoiIiwiTWFpbCI6InpodXlvdXh1QGNvcnAubmV0ZWFzZS5jb20iLCJDcmVhdGVUaW1lIjoiMjAyMy0wNi0xMyAxMTo0MjoxOSIsImlzcyI6Im1pbmltYXgifQ.VPxebiVr23nx_hcX3u79Bmt4repAyksX2W7VOejRV2TGMqIcKvjdrv5D5fMNcQjY_EUUpoPT93nrOxFO7PJVryTb86ZvDTgQY4ZzdwuI_uShtF0plUycow-vb8VUOtn3StvE7ajOfizaEESTAnjBNd9hwxBFhjPIfXAEg8LjUzuHl_Q3Z3mOViXW8bIYW-rfBKD9_jImf4e8kgqngrIi5gEyGLb_2qOrVyXsKBUWdLDTIiw63jZcOBebW_XxjgLerxcRQ7xPfwuD-kEBNeUtS1aXnzR722b1agK039BebFIZ2PPPbxG61tVhC954koI6KFMeFrr3s3Ko8P2552jsaA";
+
+    public static readonly string URL = $"https://api.minimax.chat/v1/text/chatcompletion?GroupId={GroupID}";
+}
---
+++ Assets/Scripts/MiniMaxManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http;
+using UnityEngine;
+
+public class MiniMaxManager : MonoBehaviour
+{
+
+    private void Start()
+    {
+        TryPost();
+    }
+
+    async void TryPost()
+    {
+        HttpClient client = new HttpClient();
+        HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, MiniMaxConst.URL);
+        request.Headers.Add("Authorization", MiniMaxConst.APIKEY);
+        var content = new StringContent(content: @"{
+    ""model"": ""abab5.5-chat"",
+    ""tokens_to_generate"": 3000,
+    
+    
+    ""use_standard_sse"": true,
+    ""beam_width"": 1,
+    ""prompt"": ""古斯塔夫是xxx"",
+    ""role_meta"": {
+        ""bot_name"": ""古斯塔夫"",
+        ""user_name"": ""小明""
+    },
+    ""messages"": [
+        {
+            ""sender_type"": ""USER"",
+            ""text"": ""用10种编程语言编写贪吃蛇游戏的代码""
+        }
+    ]
+}", null, "application/json");
+        request.Content = content;
+        var response = await client.SendAsync(request);
+        response.EnsureSuccessStatusCode();
+        Debug.Log(await response.Content.ReadAsStringAsync());
+    }
+}
